70793587a756316079a475af3cea3c7aedcd0d46
Chris Gramme <cgramme36@gmail.com>
2019-01-11 17:02:53 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ $(window).scroll(function () {
     var width = $(window).width();
     var scrollDistance = ($(this).scrollTop());
 
-    console.log((scrollDistance/10000));
-
 
     if(width >= 1200 && scrollDistance){
         $('#myVideo').css({'top' : ((scrollDistance/1.5)-20)+"px"});
@@ -16,5 +14,38 @@ $(window).scroll(function () {
         $('.header-graphic').css({'transform' : "scale("+(1-(scrollDistance/5000))+")"});
     }
 
+});
+
+$(".nav li").click(function(){
+    var loadPage;
+
+
+
+    if($(this).text() == "About"){
+        loadPage = "about_content";
+    }else if($(this).text() == "Bride's Party"){
+        loadPage = "bride_content";
+    }else if($(this).text() == "Groom's Party"){
+        loadPage = "groom_content";
+    }else if($(this).text() == "Registry"){
+        loadPage = "registry_content";
+    }else if($(this).text() == "Contact"){
+        loadPage = "contact_content";
+    }else{
+        loadPage = "about_content";
+    }
+
+
+  $.ajax({url: loadPage+".html", success: function(result){
+
+    $("#ajaxContent").addClass('fade-out').delay(500).queue(function(next){
+
+        $("#ajaxContent").html(result).removeClass('fade-out').addClass('fade-in');
+
+        next();
+
+    });
+
+  }});
 });
 
